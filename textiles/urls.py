2c04a477f783734508290d4a67f6dfa734cb3bb1
ebrahimasifismail <ebrahimasifismail@gmail.com>
2018-11-28 09:08:19 +0530

    functioality

--- textiles/urls.py
+++ textiles/urls.py
@@ -4,6 +4,9 @@ app_name = 'textiles'
 
 urlpatterns = [
     path('home/', views.Home.as_view(), name='home'),
+    path('saree/', views.IndexList.as_view(), name='saree'),
+    path('saree/<int:pk>/', views.saree_detail.as_view(), name='saree_detail'),
+    
     # path('', views.home, name='home-paytm'),
     # path('payment/', views.payment, name='payment'),
     # path('response/', views.response, name='response'),
