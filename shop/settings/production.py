efe8871bbf75eb6b31658c3393af370d093c37f6
ebrahimasifismail <ebrahimasifismail@gmail.com>
2019-02-05 14:56:39 +0530

    production

--- shop/settings/production.py
+++ shop/settings/production.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
