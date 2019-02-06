b60247e7ca0e0bd96c0a1266a5633f5d4eaadacc
ebrahimasifismail <ebrahimasifismail@gmail.com>
2019-02-06 14:53:07 +0530

    static files directory again changed

--- shop/settings/base.py
+++ shop/settings/base.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -160,7 +160,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 LOGOUT_REDIRECT_URL = '/login/'
